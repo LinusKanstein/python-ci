4a4734b97aa598c69703d376818dca5346d56973
Linus Kanstein <linus.kanstein@gmail.com>
2022-12-12 13:55:23 +0100

    refactored tests

--- test/test.py
+++ test/test.py
@@ -1,18 +1,18 @@
 import unittest
 
-from src.main import main
+from src import main
 
 
 class Test(unittest.TestCase):
 
     def shouldSayHello(self):
-        self.assertEqual('Hello World', main('run'))
+        self.assertEqual('Hello World', main.main('run'))
 
     def shouldSayGoodbye(self):
-        self.assertEqual('Goodbye', main('test'))
+        self.assertEqual('Goodbye', main.main('test'))
 
     def shouldNotFailForNone(self):
-        self.assertEqual('Hello World', main(None))
+        self.assertEqual('Hello World', main.main(None))
 
 
 if __name__ == '__main__':
